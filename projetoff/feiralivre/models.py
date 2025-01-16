e2c0f7ede18c3ec9a0e67dee9f66c1cf621917f0
Vinicius40028922 <magalhaesv335@gmail.com>
2025-01-16 16:26:36 -0300

    tabelas ligadas

--- projetoff/feiralivre/models.py
+++ projetoff/feiralivre/models.py
@@ -9,4 +9,13 @@ class Topic(models.Model):
     def __str__(self):
         """Devolve um representação em string do modelo. """
         return self.text
-
+    
+class Entry(models.Model):
+    """Algumacoisa"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE )
+    text= models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Devolve um representação em string do modelo. """
+        return self.text[:50] + '...'
